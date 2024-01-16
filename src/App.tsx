b9dc372bcf641ff0f899a9363f3a3914bff54855
Pedro Brasca <pedropaulobrascas@gmail.com>
2024-01-16 20:07:29 -0300

    configurando eslint e prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
+import { Button } from './components/ui/button'
 
 export function App() {
-
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className="flex h-screen items-center justify-center">
+      <Button>Olá</Button>
+    </div>
   )
 }
-
