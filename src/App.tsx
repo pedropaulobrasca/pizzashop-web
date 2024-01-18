9bac4d411b64e959a0d414e50b10e0355b00702b
Pedro Brasca <pedropaulobrascas@gmail.com>
2024-01-18 18:00:35 -0300

    cabecalho pronto

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import './global.css'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
+import { ThemeProvider } from './components/theme-provider'
 import { Toaster } from './components/ui/sonner'
 import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <RouterProvider router={router} />
-      <Toaster />
+      <ThemeProvider defaultTheme="system" storageKey="pizza.shop-theme">
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <RouterProvider router={router} />
+        <Toaster />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
