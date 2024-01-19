444e87f3a5598a6a7aef6c026688390eeb66be77
Pedro Brasca <pedropaulobrascas@gmail.com>
2024-01-19 18:51:01 -0300

    ajustes paginacao

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -7,6 +7,7 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from './ui/pagination'
+import { Separator } from './ui/separator'
 
 export interface PaginationProps {
   pageIndex: number
@@ -22,37 +23,40 @@ export function PaginationComponent({
   const pages = Math.ceil(totalCount / perPage) || 1
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-1 items-center justify-between">
       <span className="text-sm text-muted-foreground">
         Total de {totalCount} item(s)
       </span>
-      <Pagination className="relative flex items-center">
-        <div className="text-sm font-medium">
-          Página {pageIndex + 1} de {pages}
-        </div>
-        <PaginationContent>
-          <PaginationItem>
-            <PaginationPrevious href="#" />
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href="#">1</PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href="#" isActive>
-              2
-            </PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href="#">3</PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationEllipsis />
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationNext href="#" />
-          </PaginationItem>
-        </PaginationContent>
-      </Pagination>
+      <div>
+        <Pagination className="flex items-center gap-4">
+          <div className="text-sm font-medium">
+            Página {pageIndex + 1} de {pages}
+          </div>
+          <Separator orientation="vertical" className="h-6" />
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious href="#" />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">1</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#" isActive>
+                2
+              </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">3</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationNext href="#" />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      </div>
     </div>
   )
 }
