f6d12b872fbdc1e9a37d5a53d86fa39157f78ece
Pedro Brasca <pedropaulobrascas@gmail.com>
2024-01-19 21:22:57 -0300

    detalhes do pedido

---
+++ src/pages/app/orders/order-details.tsx
@@ -0,0 +1,125 @@
+import {
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+
+// export interface OrderDetailsProps {}
+
+export function OrderDetails() {
+  return (
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Pedido: d9k01j92j1dikasod</DialogTitle>
+        <DialogDescription>Detalhes do pedido</DialogDescription>
+      </DialogHeader>
+
+      <div className="space-y-6">
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableCell className="text-muted-foreground">Status</TableCell>
+              <TableCell className="flex items-center justify-end">
+                <div className="flex items-center gap-2">
+                  <span className="h-2 w-2 rounded-full bg-slate-400"></span>
+                  <span className="font-medium text-muted-foreground">
+                    Pendente
+                  </span>
+                </div>
+              </TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell className="text-muted-foreground">Cliente</TableCell>
+              <TableCell className="flex items-center justify-end">
+                <div className="flex items-center gap-2">
+                  <span className="font-medium text-muted-foreground">
+                    Pedro Paulo Brasca Trevisanuto
+                  </span>
+                </div>
+              </TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell className="text-muted-foreground">Telefone</TableCell>
+              <TableCell className="flex items-center justify-end">
+                <div className="flex items-center gap-2">
+                  <span className="font-medium text-muted-foreground">
+                    (12) 99799-9999
+                  </span>
+                </div>
+              </TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell className="text-muted-foreground">E-mail</TableCell>
+              <TableCell className="flex items-center justify-end">
+                <div className="flex items-center gap-2">
+                  <span className="font-medium text-muted-foreground">
+                    pedropaulobrasca@gmail.com
+                  </span>
+                </div>
+              </TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell className="text-muted-foreground">
+                Pedido realizado há
+              </TableCell>
+              <TableCell className="flex items-center justify-end">
+                <div className="flex items-center gap-2">
+                  <span className="font-medium text-muted-foreground">
+                    3 minutos
+                  </span>
+                </div>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Produto</TableHead>
+              <TableHead className="text-right">Quantidade</TableHead>
+              <TableHead className="text-right">Preco</TableHead>
+              <TableHead className="text-right">Subtotal</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell>Pizza Peperoni G</TableCell>
+              <TableCell className="text-right">2</TableCell>
+              <TableCell className="text-right">R$ 65,00</TableCell>
+              <TableCell className="text-right">R$ 130,00</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Pizza choc. c/ morango G</TableCell>
+              <TableCell className="text-right">2</TableCell>
+              <TableCell className="text-right">R$ 65,00</TableCell>
+              <TableCell className="text-right">R$ 130,00</TableCell>
+            </TableRow>
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TableCell colSpan={3}>Total do pedido</TableCell>
+              <TableCell className="text-right font-medium">
+                R$ 260,00
+              </TableCell>
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </div>
+    </DialogContent>
+  )
+}
